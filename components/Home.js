c77cd88d54dfc000dc945f7f0197afaf039bbbca
Blankaert <blankaertlouis@outlook.com>
2024-02-08 12:26:15 +0100

    first init

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
 
   const [moviesData, setMoviesData] = useState([]);
   useEffect(() => {
-    fetch("mymoviz-backend-roan-two.vercel.app")
+    fetch("mymoviz-backend-roan-two.vercel.app/movies")
       .then((response) => response.json())
       .then((data) => {
         const formattedData = data.movies.map((movie) => {
